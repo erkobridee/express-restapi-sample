c6e1c22a4726d5ebb2fde0a77dc3bbce93cf38ac
Erko Bridee <erko.bridee@gmail.com>
2020-04-16 09:44:04 +0000

    add initial express.js code

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Basic example of REST API using the [express.js](https://expressjs.com/)
 
 coded at: [repl.it](https://repl.it/@erkobridee/express-restapi-sample)
+
+## Useful links:
+
+- [Express](https://expressjs.com/)
+  - [Express v4 docs](https://expressjs.com/en/4x/api.html)
+- [Creating RESTful APIs with Express 4 | SitePoint](https://www.sitepoint.com/creating-restful-apis-express-4/)
+- [Node.js and Express Tutorial: Building and Securing RESTful APIs | Auth0](https://auth0.com/blog/node-js-and-express-tutorial-building-and-securing-restful-apis/)
--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('TODO: define the code');
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+const staticPublicOptions = {
+	dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+};
+app.use(express.static('public', staticPublicOptions))
+
+app.get('/', (req, res) => {
+  // res.send('Hello Express app!');
+	res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello Express app!' });
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
